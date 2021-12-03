1f7c7c051dc83ad68c2001836f7deab6702fd19d
Oubi <lito5443@gmail.com>
2021-12-03 12:13:48 +0200

    Поправлены цвета, заменены дефолт шрифты

--- App.js
+++ App.js
@@ -1,7 +1,14 @@
 import React, {useState, useEffect} from 'react';
-import { StyleSheet, Text, TextInput, View, Image} from 'react-native';
+import { StyleSheet, Text, TextInput, View, Image, StatusBar} from 'react-native';
 import { LinearGradient } from "expo-linear-gradient"
+import {useFonts} from "expo-font";
 
+
+const customFonts = {
+  QuicksandBold: require("./assets/Quicksand-Bold.ttf"),
+  QuicksandRegular: require("./assets/Quicksand-Regular.ttf"),
+  QuicksandSemiBold: require("./assets/Quicksand-SemiBold.ttf"),
+};
 
 export default function App() {
   return <AppScreen/>
@@ -10,6 +17,7 @@ export default function App() {
 const AppScreen = () => {
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
+  const [isFontLoaded] = useFonts(customFonts);
 
   const getWeatherFromApi = async () => {
     try {
@@ -36,15 +44,19 @@ const AppScreen = () => {
       </View>
     )
   } else {
-    
+    console.log('font:',isFontLoaded, isLoading)
     console.log('LOADED!')
     let maxMinTemp = [Math.round(data.forecast.forecastday[0].day.maxtemp_c),Math.round(data.forecast.forecastday[0].day.mintemp_c)]
     return(
         <LinearGradient
         style={styles.container}
         colors={['#7161ef', 'transparent']}
+        locations={[0.3,1]}
         start={{x:0.5, y:0}}
         end={{x:0.7, y:1}}>
+        <StatusBar
+        animated={true}
+        backgroundColor="#7161ef"/>
             <HomeSearch/>
             <HomeYesterdayWeather data={data} maxMinTemp={maxMinTemp}/>
             <HomeYesterdayWeatherAdditional data={data} maxMinTemp={maxMinTemp}/>
@@ -71,7 +83,7 @@ const HomeSearch = () => {
   }
 
   return(
-    <View style={{flex: 69, justifyContent:'flex-end', alignItems: 'center', flexDirection: 'row', marginTop: 30}}>
+    <View style={{flex: 69, justifyContent:'flex-end', alignItems: 'center', flexDirection: 'row', marginTop: 5}}>
       <TextInput style={styles.input} onChangeText={onChangeText} selectionColor={'rgba(256, 256, 256, 0.75)'} placeholder={'Location...'} placeholderTextColor={'rgba(256, 256, 256, 0.45)'}/>
     </View>
   )
@@ -83,19 +95,19 @@ const HomeYesterdayWeather = ({data, maxMinTemp}) => {
  
   return(
     <View style={{flex: 276, justifyContent: 'center', alignItems: 'center', paddingVertical: 10}}>
-      <Text style={[styles.centertext,{fontWeight: 'bold', fontSize: 16}]}>{data.location.name}, {data.location.region}</Text>
+      <Text style={[styles.centertext,{fontSize: 16, fontFamily: 'QuicksandBold'}]}>{data.location.name}, {data.location.region}</Text>
 
-      <Text style={styles.centertext}>{data.location.localtime}</Text>
+      <Text style={[styles.centertext,{fontFamily: 'QuicksandRegular'}]}>{data.location.localtime}</Text>
 
       <View style={{flex:2, flexDirection: 'row', alignItems: 'center'}}>
-        <Image source={getWeatherImgUri(data.forecast.forecastday[0].day.condition.icon)} style={{width: 64, height: 64}}></Image>
-        <Text style={{textAlignVertical: 'center', color: 'white', fontSize: 60}}>{Math.round(data.current.temp_c)}°</Text>
+        <Image tintColor={'white'} source={getWeatherImgUri(data.forecast.forecastday[0].day.condition.icon)} style={{width: 64, height: 64}}></Image>
+        <Text style={{textAlignVertical: 'center', color: 'white', fontSize: 60, fontFamily: 'QuicksandSemiBold'}}>{Math.round(data.current.temp_c)}°</Text>
       </View>
       
 
-      <Text style={[styles.centertext,{fontWeight: 'bold'}]}>{maxMinTemp[0]}°/{maxMinTemp[1]}° Feels like {Math.round(data.current.feelslike_c)}°</Text>
+      <Text style={[styles.centertext,{fontFamily: 'QuicksandBold'}]}>{maxMinTemp[0]}°/{maxMinTemp[1]}° Feels like {Math.round(data.current.feelslike_c)}°</Text>
       
-      <Text style={[styles.centertext,{fontWeight: 'bold', paddingBottom: 20}]}>{data.forecast.forecastday[0].day.condition.text}</Text>
+      <Text style={[styles.centertext,{fontFamily: 'QuicksandBold', paddingBottom: 20}]}>{data.forecast.forecastday[0].day.condition.text}</Text>
     </View>
   )
 }
@@ -118,17 +130,17 @@ const getUVlevel = () => {
 
   return(
     <View style={{flex: 135}}>
-      <Text style={{textAlign:'right', paddingHorizontal: 30, color: 'white',paddingBottom:3}}>Yesterday: {maxMinTemp[0]}°/{maxMinTemp[1]}°</Text>
+      <Text style={{textAlign:'right', paddingHorizontal: 30, color: 'white',paddingBottom:3, fontFamily: 'QuicksandSemiBold'}}>Yesterday: {maxMinTemp[0]}°/{maxMinTemp[1]}°</Text>
       <View style={styles.bordercontainer}>
         <View style={{flex: 1, margin: 5, alignItems: 'center', flexDirection: 'row', justifyContent: 'center'}}>
           <Image source={{uri: 'https://cdn-icons-png.flaticon.com/512/727/727802.png'}}  resizeMode={'contain'} tintColor={'white'} style={{width: 32, height: 32,alignSelf: 'center'}}></Image>
-          <Text style={{textAlignVertical: 'center', color: 'white'}}>Precipitation{"\n"}{data.current.humidity}%</Text>
+          <Text style={{textAlignVertical: 'center', color: 'white', fontFamily: 'QuicksandSemiBold'}}>Precipitation{"\n"}{data.current.humidity}%</Text>
         </View>
         <View style={{height: "45%", width: 1,backgroundColor:'white', alignSelf: 'center', borderRadius: 10}}></View>
           
         <View style={{flex: 1, margin: 5, alignItems: 'center', flexDirection: 'row', justifyContent: 'center'}}>
           <Image source={{uri: 'https://cdn-icons-png.flaticon.com/512/106/106061.png'}}  resizeMode={'contain'} tintColor={'white'} style={{width: 32, height: 32,alignSelf: 'center', marginRight: 5}}></Image>
-          <Text style={{textAlignVertical: 'center', color: 'white'}}>UV Index{"\n"}{getUVlevel()}</Text>
+          <Text style={{textAlignVertical: 'center', color: 'white', fontFamily: 'QuicksandSemiBold'}}>UV Index{"\n"}{getUVlevel()}</Text>
         </View>
       </View>
     </View>
@@ -153,7 +165,7 @@ const HomeFullDayWeather = ({data, maxMinTemp}) => {
 
   return (
     <View style={{flex: 260}}>
-      <Text style={{textAlign:'left', paddingHorizontal: 30, color: 'white',paddingBottom:3}}>Hourly</Text>
+      <Text style={{textAlign:'left', paddingHorizontal: 30, color: 'white',paddingBottom:3, fontFamily: 'QuicksandSemiBold'}}>Hourly</Text>
       <View style={styles.bordercontainer}>
         <WeatherColum dayData={data.forecast.forecastday[dayHour[0][0]].hour[dayHour[0][1]]} maxMinTemp={maxMinTemp}/>
         <WeatherColum dayData={data.forecast.forecastday[dayHour[1][0]].hour[dayHour[1][1]]} maxMinTemp={maxMinTemp}/>
@@ -175,11 +187,11 @@ const WeatherColum = ({ dayData, maxMinTemp}) => {
   console.log('WeatherColum:',dayData.time)
   return(
     <View style={{flex: 1, margin: 5, alignItems: 'center'}}>
-      <Text style={[styles.centertext, {flex: 0.5, textAlignVertical: 'bottom', marginTop: 10, fontWeight: 'bold'}]}>{dayData.time.slice(-5)}</Text>
+      <Text style={[styles.centertext, {flex: 0.5, textAlignVertical: 'bottom', marginTop: 10, fontFamily: 'QuicksandBold'}]}>{dayData.time.slice(-5)}</Text>
 
       <View style={styles.centertext}>
-        <Image source={getWeatherImgUri(dayData.condition.icon)}  resizeMode={'contain'}  style={{flex: 2, width: 64, height: 64, alignSelf: 'center'}}></Image>
-        <Text style={[styles.centertext,{fontSize: 10, textAlign: 'center', textAlignVertical: 'top'}]}> {Math.max(dayData.chance_of_rain, dayData.chance_of_snow)}%</Text>
+        <Image tintColor={'white'} source={getWeatherImgUri(dayData.condition.icon)}  resizeMode={'contain'}  style={{flex: 2, width: 64, height: 64, alignSelf: 'center'}}></Image>
+        <Text style={[styles.centertext,{fontSize: 10, textAlign: 'center', textAlignVertical: 'top' }]}> {Math.max(dayData.chance_of_rain, dayData.chance_of_snow)}%</Text>
       </View>
 
       <View style={{
@@ -189,7 +201,7 @@ const WeatherColum = ({ dayData, maxMinTemp}) => {
         flex: 1.5,
         marginVertical: '30%'
         }}>
-        <Text style={{alignSelf: 'flex-end', color: 'white', fontSize: 18}}> {Math.round(dayData.temp_c)}°</Text>
+        <Text style={{alignSelf: 'flex-end', color: 'white', fontSize: 22, fontFamily: 'QuicksandSemiBold'}}> {Math.round(dayData.temp_c)}°</Text>
         <View style={[{width: 4, backgroundColor:'white', borderRadius: 10},getTempGraphStyle(Math.round(dayData.temp_c))]}></View>
       </View>
     </View>
@@ -210,6 +222,7 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     borderColor: 'rgba(256, 256, 256, 0.35)',
     color: 'rgba(256, 256, 256, 0.45)',
+    fontFamily: 'QuicksandRegular'
   },
   bordercontainer: {
     flex: 1,
@@ -222,7 +235,7 @@ const styles = StyleSheet.create({
   centertext:{
     flex: 1,
     textAlignVertical: 'center',
-    color: 'white'
+    color: 'white',
   }
 });
 ///#a0c6f6
